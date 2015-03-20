0dcaa260df3a2457eb8174ffd8e71dd2202fe475
timothy-clifford <tim.clifford@pawsforlife.com.au>
2015-03-21 00:34:22 +1100

    adding round and team into

--- app/app.js
+++ app/app.js
@@ -8,6 +8,8 @@ angular.module('tipper2', [
 	'tipper2.draw.services',
 	'tipper2.comp.services',
 	'tipper2.comp',
+	'tipper2.team',
+	'tipper2.round',
 	'tipper2.draw'
 ]).
 
@@ -19,6 +21,14 @@ config(['$routeProvider', function($routeProvider) {
 	$routeProvider.when('/comp/ladder', {
 	    templateUrl: 'comp/ladder.html',
 	    controller: 'LadderCtrl'
+	});
+	$routeProvider.when('/team/:teamName', {
+		templateUrl: 'team/team.html',
+		controller: 'TeamCtrl'
+	});
+	$routeProvider.when('/round/:roundName', {
+		templateUrl: 'round/round.html',
+		controller: 'RoundCtrl'
 	});
   	$routeProvider.otherwise({redirectTo: '/comp/ladder'});
 
