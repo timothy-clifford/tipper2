148666528e6bd621bd4b8aedcccd225dc64c5493
Timothy Clifford <timothy.clifford@yahoo.com>
2015-05-05 09:19:01 +1000

    adding previous ranking

--- app/comp/ladder.controller.js
+++ app/comp/ladder.controller.js
@@ -12,6 +12,7 @@ angular.module('tipper2.comp', ['ngRoute', 'ui.grid'])
 
 	LadderSvc.query().$promise.then( function(data) {
 		$scope.ladderOptions.data = data;
+		$scope.calculateLadderChange(data);
 
 		$scope.ladderOptions.columnDefs.push({name: 'name'});
 		var roundsCompleted = $scope.ladderOptions.data[0].results.length;
@@ -19,26 +20,33 @@ angular.module('tipper2.comp', ['ngRoute', 'ui.grid'])
 			// this will work until the finals...
 			$scope.ladderOptions.columnDefs.push( {name: 'round ' + (i + 1), field: 'results[' + i + '].score'});
 		}
-		// find the movement from the last round, ie. rank up 2, down 1, etc
-		// need to find the rank for the current round (ordered list of names)
-		var rankedCurrentRound = $scope.ladderOptions.data.sortBy(function(n) {
-			if (n.results.length > 0) {
-				return n.results[n.results.length - 1].cumulativeTotal;
-			}
-		}, true);
-		// need to find the rank for the previous round (ordered list of names)
-		var rankedPreviousRound = $scope.ladderOptions.data.sortBy(function(n) {
-			if (n.results.length > 1)
-			return n.results[n.results.length - 2].cumulativeTotal;
-		}, true);
-		// for each name, compare the current rank to the previous, add to the player object
-		$scope.ladderOptions.data.each(function(n) {
-			var currentRank = rankedCurrentRound.findIndex({ name: n.name});
-			var previousRank = rankedPreviousRound.findIndex({ name: n.name});
-			n.change = currentRank - previousRank;
-		});
+
 		$scope.ladderOptions.columnDefs.push({name: 'change'});	
 		$scope.ladderOptions.columnDefs.push({name: 'total'});
 	});
+
+	$scope.calculateLadderChange = function(data) {
+		// create an ordered array of scores.
+		var uniqueTotals = [];
+		data.each(function(n) {
+			uniqueTotals.push(n.total);
+		});
+		uniqueTotals = uniqueTotals.unique();
+
+		var uniquePreviousTotals = [];
+		data.each(function (n) {
+			uniquePreviousTotals.push(n.results[ n.results.length - 2 ].cumulativeTotal);
+		});
+		uniquePreviousTotals = uniquePreviousTotals.unique();
+
+		// for each name, compare the current rank to the previous, add to the player object
+		$scope.ladderOptions.data.each(function(n) {
+			var currentRank = uniqueTotals.findIndex(n.total);
+			var blah = n.results[ n.results.length - 2 ].cumulativeTotal;
+			var previousRank = uniquePreviousTotals.findIndex(blah);
+			// rank is an index, convert to number
+			n.change = (previousRank - currentRank);
+		});
+	}
 
 }]);
